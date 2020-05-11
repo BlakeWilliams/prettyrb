bd9da6b09ee41d7aab66bb6ab8c424785893ec85
Blake Williams <blake@blakewilliams.me>
2020-05-10 20:29:45 -0400

    write files

--- lib/prettyrb/cli.rb
+++ lib/prettyrb/cli.rb
@@ -3,18 +3,26 @@ require "thor"
 module Prettyrb
   class CLI < Thor
     desc "format [FILE]", "Ruby file to prettify"
-    option :write, type: :boolean
-    method_option :files, type: :array
-    def format(*files)
+    def print(file)
       content = File.read(file)
       formatted_content = Prettyrb::Formatter.new(content).format
 
-      if options[:write]
-        File.open(file, 'w') do |f|
-          f.write(formatted_content)
+      puts formatted_content
+    end
+
+    desc "write [FILES]", "Write prettified Ruby files"
+    def write(*files)
+      files.each do |file|
+        content = File.read(file)
+        formatted_content = Prettyrb::Formatter.new(content).format
+
+        if options[:write]
+          File.open(file, 'w') do |f|
+            f.write(formatted_content)
+          end
+        else
+          puts formatted_content
         end
-      else
-        puts formatted_content
       end
     end
   end
