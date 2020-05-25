fc86340f0484faa2ce78163fe007a090a8d74021
Blake Williams <blake@blakewilliams.me>
2020-05-25 14:28:45 -0400

    Add some basic writer tests

--- test/prettyrb/writer_test.rb
+++ test/prettyrb/writer_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
-class PrettyrbTest < Minitest::Test
-  def test_concat_appends_values
-    # Writer.new()
+module Prettyrb
+  class WriterTest < Minitest::Test
+    def test_concat_appends_values
+      builder = Document::Concat.new("foo", "bar")
+      assert_equal "foobar", Writer.new(builder).to_s
+    end
+
+    def test_indent_increases_indentation_for_newlines
+      builder = Document::Indent.new(Document::Hardline.new, "foo")
+      assert_equal "\n  foo", Writer.new(builder).to_s
+    end
+    
+    def test_dedent_decreases_indentation_for_newlines
+      builder = Document::Indent.new(
+        Document::Hardline.new,
+        "foo",
+        Document::Dedent.new(
+          Document::Hardline.new,
+          "bar",
+        )
+      )
+      assert_equal "\n  foo\nbar", Writer.new(builder).to_s
+    end
   end
 end
